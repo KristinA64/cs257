343d8d3b8ac35d903a49d5f796cab09e29c399e6
Jayti Arora <jaytiarora@Jaytis-MacBook-Pro.local>
2021-10-02 21:44:00 -0500

    Added to books.py

--- books/books.py
+++ books/books.py
@@ -1,12 +1,31 @@
 import argparse
 import booksdatasource
 
-
+#initialize the parser
 parser = argparse.ArgumentParser()
-parser.parse_args()
-parser.add_argument("-a", help="Given a string, print the list of books in the dataset whose authors name contains that string.")
-parser.add_argument("-b", help="Given a string, print the list of books in the dataset whose titles contain that string.")
-parser.add_argument("-y", help="Given 2 integers, print the list of books in the dataset which were published between the years of the integers.")
-parser.add_argument("-u", help="Prints the usage statement")
 
-args = parsers.parse_args()
+#add parameters
+parser.add_argument("-a", '--author', help="Given a string, prints the list of books in the dataset whose authors name contains that string, sorted by last name.")
+parser.add_argument("-b", "--book", help="Given a string, prints the list of books in the dataset whose titles contain that string.")
+parser.add_argument("-y","--years", help="Given 2 integers, prints the list of books in the dataset which were published between the years of the integers inclusive.", type=int)
+parser.add_argument("-u", "--usage", help="Prints the usage statement")
+
+#parse the arguments
+args = parser.parse_args()
+
+print(args) #prints args correctly
+
+#send in args
+if "--book" != None:
+  print("1")
+  #call booksdata source books function on parameter --book
+  #need to add a search text arguement and if that is not none, also send that in
+elif "--author" != None:
+  print("2")
+  #call booksdata source author function on parameter --author
+elif "--year" != None:
+  print("3")
+  #year is only 1 arguement so how do we get end year?
+elif "--usage" != None:
+  print("4)
+  #print usage.txt - do we need to import it?
