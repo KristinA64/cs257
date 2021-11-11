aebe12ba34730bf3f3888a0f3c37220f86012a58
XinyanXiang <52022947+XinyanXiang@users.noreply.github.com>
2021-11-10 19:18:20 -0600

    add database

---
+++ webapp/data/grammy-schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE category (
+    id INTEGER,
+	category TEXT
+);
+-- \copy category FROM 'category.csv' DELIMITER ',' CSV NULL AS 'NULL'
+
+
+CREATE TABLE award_year (
+	id INTEGER,
+	year INTEGER,
+    award_title TEXT
+);
+-- \copy award_year FROM 'award_year.csv' DELIMITER ',' CSV NULL AS 'NULL'
+
+CREATE TABLE nominee_award (
+	id INTEGER,
+	award_year_id INTEGER,
+    category_id INTEGER,
+    nominee_id INTEGER
+);
+-- \copy nominee_award FROM 'nominee_award.csv' DELIMITER ',' CSV NULL AS 'NULL'
+
+
+
+CREATE TABLE nominee_information (
+	id INTEGER,
+	nominee_name TEXT,
+    artist TEXT,
+    workers TEXT,
+    img TEXT
+);
+-- \copy nominee_information FROM 'nominee_information.csv' DELIMITER ',' CSV NULL AS 'NULL'
+
