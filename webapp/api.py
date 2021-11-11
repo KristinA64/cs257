e988c8eb9a3f661e98e78f33f0db3d2ea8966411
XinyanXiang <52022947+XinyanXiang@users.noreply.github.com>
2021-11-10 20:11:43 -0600

    add sql

--- webapp/api.py
+++ webapp/api.py
@@ -63,50 +63,77 @@ def get_titles():
 
 
 
-@api.route('/authors/') 
-def get_authors():
-    ''' Returns a list of all the authors in our database. See
-        get_author_by_id below for description of the author
-        resource representation.
+# @api.route('/authors/') 
+# def get_authors():
+#     ''' Returns a list of all the authors in our database. See
+#         get_author_by_id below for description of the author
+#         resource representation.
 
-        By default, the list is presented in alphabetical order
-        by surname, then given_name. You may, however, use
-        the GET parameter sort to request sorting by birth year.
+#         By default, the list is presented in alphabetical order
+#         by surname, then given_name. You may, however, use
+#         the GET parameter sort to request sorting by birth year.
 
-            http://.../authors/?sort=birth_year
+#             http://.../authors/?sort=birth_year
 
-        Returns an empty list if there's any database failure.
+#         Returns an empty list if there's any database failure.
+#     '''
+#     query = '''SELECT id, given_name, surname, birth_year, death_year
+#                FROM authors ORDER BY '''
+
+#     sort_argument = flask.request.args.get('sort')
+#     if sort_argument == 'birth_year':
+#         query += 'birth_year'
+#     else:
+#         query += 'surname, given_name'
+
+#     author_list = []
+#     try:
+#         connection = get_connection()
+#         cursor = connection.cursor()
+#         cursor.execute(query, tuple())
+#         for row in cursor:
+#             author = {'id':row[0],
+#                       'given_name':row[1],
+#                       'surname':row[2],
+#                       'birth_year':row[3],
+#                       'death_year':row[4]}
+#             author_list.append(author)
+#         cursor.close()
+#         connection.close()
+#     except Exception as e:
+#         print(e, file=sys.stderr)
+
+#     return json.dumps(author_list)
+
+# @api.route('/books/author/<author_id>')
+# def get_books_for_author(author_id):
+#     query = '''SELECT books.id, books.title, books.publication_year
+#                FROM books, authors, books_authors
+#                WHERE books.id = books_authors.book_id
+#                  AND authors.id = books_authors.author_id
+#                  AND authors.id = %s
+#                ORDER BY books.publication_year'''
+#     book_list = []
+#     try:
+#         connection = get_connection()
+#         cursor = connection.cursor()
+#         cursor.execute(query, (author_id,))
+#         for row in cursor:
+#             book = {'id':row[0], 'title':row[1], 'publication_year':row[2]}
+#             book_list.append(book)
+#         cursor.close()
+#         connection.close()
+#     except Exception as e:
+#         print(e, file=sys.stderr)
+
+#     return json.dumps(book_list)
+
+@api.route('/grammys/<grammy_id>')
+def get_books_for_author(grammy_id):
+
+    query = '''SELECT award_year.award_title, category.category,nominee_information.nominee_name
+    FROM award_year, category,nominee_information,
     '''
-    query = '''SELECT id, given_name, surname, birth_year, death_year
-               FROM authors ORDER BY '''
-
-    sort_argument = flask.request.args.get('sort')
-    if sort_argument == 'birth_year':
-        query += 'birth_year'
-    else:
-        query += 'surname, given_name'
-
-    author_list = []
-    try:
-        connection = get_connection()
-        cursor = connection.cursor()
-        cursor.execute(query, tuple())
-        for row in cursor:
-            author = {'id':row[0],
-                      'given_name':row[1],
-                      'surname':row[2],
-                      'birth_year':row[3],
-                      'death_year':row[4]}
-            author_list.append(author)
-        cursor.close()
-        connection.close()
-    except Exception as e:
-        print(e, file=sys.stderr)
-
-    return json.dumps(author_list)
-
-@api.route('/books/author/<author_id>')
-def get_books_for_author(author_id):
     query = '''SELECT books.id, books.title, books.publication_year
                FROM books, authors, books_authors
                WHERE books.id = books_authors.book_id
